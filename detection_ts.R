310b3ea6c96713416d5d6e234e6ce3fd8f380c18
mhpob <mikeob9@gmail.com>
2016-11-15 14:18:09 -0500

    Add messy code for cruise report data summary.

--- detection_ts.R
+++ detection_ts.R
@@ -8,7 +8,7 @@ detects <- filter(detects,
 
 j <- distinct(detects, transmitter, station, agg.date) %>%
   group_by(transmitter, agg.date)
-k <- filter(j, grepl('Middle', station)) %>%
+k <- filter(j, grepl('Inner', station)) %>%
   summarize(n=n())
 
 
@@ -17,3 +17,15 @@ ggplot() + geom_smooth(data = k, aes(x = agg.date, y = n)) +
   geom_point(data = k, aes(x = agg.date, y = n)) +
   ylim(c(1,3)) +
   facet_wrap(~transmitter)
+
+
+bsb <- vemsort('p:/obrien/biotelemetry/detections/offshore md/marine mammal monitoring')
+bsb <- filter(bsb,
+                  transmitter %in% paste0('A69-1601-', seq(44950, 44994, 1)))
+
+
+
+detects <- filter(detects,
+              transmitter %in% paste0('A69-1601-', seq(44950, 44994, 1)),
+              date.local < ymd_hms('2016-08-03 00:00:00', tz = 'America/New_York'))
+j <- group_by(detects, station) %>% summarize(test = length(unique(transmitter)))
