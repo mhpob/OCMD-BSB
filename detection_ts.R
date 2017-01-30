275f6704a1297be1f6eaf6f16bba03a37cc0ac95
mhpob <mikeob9@gmail.com>
2017-01-30 16:20:53 -0500

    Label according to arrays, plot date v detections/hr.

--- detection_ts.R
+++ detection_ts.R
@@ -1,22 +1,35 @@
-library(TelemetryR); library(lubridate); library(dplyr)
+library(TelemetryR); library(ggplot2); library(lubridate); library(dplyr)
 detects <- vemsort('p:/obrien/biotelemetry/ocmd-bsb/receiver logs')
 detects <- filter(detects,
-                  transmitter %in% paste0('A69-1601-', seq(44950, 44994, 1))) %>%
-  mutate(agg.date = ceiling_date(date.local, unit = 'hour'))
+                  transmitter %in%
+                    paste0('A69-1601-', seq(44950, 44994, 1))) %>%
+  mutate(agg.date = ceiling_date(date.local, unit = 'hour'),
+         array = ifelse(grepl('Outer', station), 'Outer',
+                 ifelse(grepl('Middle', station), 'Middle',
+                              'Inner')))
 
 
+# j <- distinct(detects, transmitter, station, agg.date) %>%
+#   group_by(transmitter, agg.date)
+# k <- filter(j, grepl('Inner', station)) %>%
+#   summarize(n=n())
 
-j <- distinct(detects, transmitter, station, agg.date) %>%
-  group_by(transmitter, agg.date)
-k <- filter(j, grepl('Inner', station)) %>%
-  summarize(n=n())
+# ggplot() + geom_smooth(data = k, aes(x = agg.date, y = n)) +
+#   geom_point(data = k, aes(x = agg.date, y = n)) +
+#   ylim(c(1,3)) +
+#   facet_wrap(~transmitter)
 
+j <- distinct(detects, transmitter, station, agg.date, .keep_all =T) %>%
+  group_by(transmitter, array, agg.date) %>%
+  summarize(n = n())
 
-library(ggplot2)
-ggplot() + geom_smooth(data = k, aes(x = agg.date, y = n)) +
-  geom_point(data = k, aes(x = agg.date, y = n)) +
-  ylim(c(1,3)) +
-  facet_wrap(~transmitter)
+ggplot() + geom_smooth(data = j, aes(x = agg.date, y = n)) +
+  geom_point(data = j, aes(x = agg.date, y = n)) +
+  facet_wrap(~array) +
+  scale_y_continuous(breaks = c(1, 2, 3)) +
+  labs(x = 'Date', y = expression(Receivers~hour^{-1})) +
+  theme_bw()
+
 
 
 bsb <- vemsort('p:/obrien/biotelemetry/detections/offshore md/marine mammal monitoring')
