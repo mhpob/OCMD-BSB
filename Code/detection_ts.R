1360101c1c976c83074230524b3d0f1c0a5b51ed
mhpob <mikeob9@gmail.com>
2017-07-31 16:15:54 -0400

    Plot number of fish per array vs time.

--- Code/detection_ts.R
+++ Code/detection_ts.R
@@ -1,12 +1,15 @@
 library(TelemetryR); library(ggplot2); library(lubridate); library(dplyr)
-detects <- vemsort('p:/obrien/biotelemetry/ocmd-bsb/receiver logs')
+detects <- vemsort('P:/OBrien/Biotelemetry/Detections/Offshore MD/BSB Noise Impact')
 detects <- filter(detects,
                   transmitter %in%
-                    paste0('A69-1601-', seq(44950, 44994, 1))) %>%
-  mutate(agg.date = ceiling_date(date.local, unit = 'hour'),
-         array = ifelse(grepl('Outer', station), 'Outer',
+                    paste0('A69-1601-', seq(44950, 44994, 1)),
+                  date.local <= '2016-12-31',
+                  date.local >= '2016-06-12') %>%
+  mutate(agg.date = floor_date(date.local, unit = 'day'),
+         array = ifelse(grepl('Outer', station), 'Southern',
                  ifelse(grepl('Middle', station), 'Middle',
-                              'Inner')))
+                              'Northern')),
+         array = ordered(array, levels = c('Northern', 'Middle', 'Southern')))
 
 
 # j <- distinct(detects, transmitter, station, agg.date) %>%
@@ -19,26 +22,46 @@ detects <- filter(detects,
 #   ylim(c(1,3)) +
 #   facet_wrap(~transmitter)
 
-j <- distinct(detects, transmitter, station, agg.date, .keep_all =T) %>%
-  group_by(transmitter, array, agg.date) %>%
-  summarize(n = n())
+# j <- distinct(detects, transmitter, station, agg.date, .keep_all =T) %>%
+#   group_by(transmitter, array, agg.date) %>%
+#   summarize(n = n())
+#
+# ggplot() + geom_smooth(data = j, aes(x = agg.date, y = n)) +
+#   geom_point(data = j, aes(x = agg.date, y = n)) +
+#   facet_wrap(~array) +
+#   scale_y_continuous(breaks = c(1, 2, 3)) +
+#   labs(x = 'Date', y = expression(Receivers~hour^{-1})) +
+#   theme_bw()
+date.fill <- data.frame(agg.date = rep(seq(as.Date('2016-06-12'),
+                                       as.Date('2016-12-31'),
+                                       by = 'day'),
+                                       each = 3),
+                        array = ordered(rep(c('Northern', 'Middle', 'Southern'),
+                                    times = 203),
+                                    levels = c('Northern', 'Middle', 'Southern')),
+                        stringsAsFactors = F)
+date.fill$agg.date <- ymd(date.fill$agg.date, tz = 'America/New_York')
 
-ggplot() + geom_smooth(data = j, aes(x = agg.date, y = n)) +
-  geom_point(data = j, aes(x = agg.date, y = n)) +
-  facet_wrap(~array) +
-  scale_y_continuous(breaks = c(1, 2, 3)) +
-  labs(x = 'Date', y = expression(Receivers~hour^{-1})) +
+j <- distinct(detects, transmitter, array, agg.date, .keep_all =T) %>%
+  group_by(array, agg.date) %>%
+  summarize(n = n()) %>%
+  full_join(date.fill) %>%
+  mutate(n = ifelse(is.na(n), 0, n))
+
+ggplot() + geom_line(data = j, aes(x = agg.date, y = n)) +
+  facet_wrap(~array)+
+  lims(y = c(0, 15)) +
+  labs(x = 'Date', y = 'Fish detected') +
+  scale_x_datetime(date_breaks = '1 month', date_labels = '%b', minor_breaks = NULL) +
   theme_bw()
 
-
-
-bsb <- vemsort('p:/obrien/biotelemetry/detections/offshore md/marine mammal monitoring')
-bsb <- filter(bsb,
-                  transmitter %in% paste0('A69-1601-', seq(44950, 44994, 1)))
-
-
-
-detects <- filter(detects,
-              transmitter %in% paste0('A69-1601-', seq(44950, 44994, 1)),
-              date.local < ymd_hms('2016-08-03 00:00:00', tz = 'America/New_York'))
-j <- group_by(detects, station) %>% summarize(test = length(unique(transmitter)))
+# bsb <- vemsort('p:/obrien/biotelemetry/detections/offshore md/marine mammal monitoring')
+# bsb <- filter(bsb,
+#                   transmitter %in% paste0('A69-1601-', seq(44950, 44994, 1)))
+#
+#
+#
+# detects <- filter(detects,
+#               transmitter %in% paste0('A69-1601-', seq(44950, 44994, 1)),
+#               date.local < ymd_hms('2016-08-03 00:00:00', tz = 'America/New_York'))
+# j <- group_by(detects, station) %>% summarize(test = length(unique(transmitter)))
