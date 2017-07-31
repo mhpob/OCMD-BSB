d171e19f54595a17c2a54bdd3bd253203f55a234
mhpob <mikeob9@gmail.com>
2017-07-31 16:16:27 -0400

    Add contrasts.

--- Code/movement_index.R
+++ Code/movement_index.R
@@ -4,7 +4,7 @@ library(TelemetryR); library(lubridate); library(dplyr)
 source('p:/obrien/biotelemetry/ocmd-bsb/code/bsb_detection_import.R')
 
 dets <- bsb.detects %>%
-  filter(date.utc < '2016-11-11') %>%
+  filter(date.utc <= '2016-12-31') %>%
   mutate(hr_floor = floor_date(date.utc, unit = 'hour'))
 
 dets <- split(dets, factor(dets$transmitter))
@@ -41,7 +41,7 @@ ggplot() + geom_point(data = dets, aes(x = date, y = m.index)) +
 
 
 max.date <- bsb.detects %>%
-  filter(date.utc < '2016-11-11') %>%
+  filter(date.utc <= '2016-12-31') %>%
   group_by(transmitter) %>%
   summarize(max = max(date.utc))
 
@@ -59,6 +59,13 @@ hermine_dets <- left_join(dets, max.date) %>%
   group_by(transmitter, h.event, hermine.exit) %>%
   summarize(avg.move = mean(m.index))
 
+library(readxl)
+tagdat <- read_excel('p:/obrien/biotelemetry/ocmd-bsb/data/taggingdata_ocbsb_16.xlsx')
+
+hermine_dets <- left_join(hermine_dets,
+                          tagdat[, c('Transmitter', 'Release Array')],
+                          by = c('transmitter' = 'Transmitter'))
+
 ggplot() +
   geom_violin(data = hermine_dets, aes(x = h.event, y = avg.move,
                                        fill = hermine.exit),
@@ -69,3 +76,15 @@ ggplot() +
              binwidth = 0.25) +
   theme(legend.position = c(0.9, 0.75))
 
+# Contrasts
+fate.cons <- lm(log1p(avg.move) ~ hermine.exit +
+                 `Release Array`, subset = (h.event == 'Pre'),
+                data = hermine_dets)
+summary(fate.cons)
+anova(fate.cons)
+
+move.cons <- lm(avg.move ~ h.event +
+                  `Release Array` , subset = (hermine.exit == 'Remained'),
+                data = hermine_dets)
+summary(move.cons)
+anova(move.cons)
