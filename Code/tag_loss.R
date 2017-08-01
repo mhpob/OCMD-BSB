3e430524e4527485b37b45626ac9353d5d51f6b7
mhpob <mikeob9@gmail.com>
2017-08-01 09:27:24 -0400

    Select correct dates, begin manuscript plotting.

--- Code/tag_loss.R
+++ Code/tag_loss.R
@@ -2,23 +2,25 @@ library(readxl)
 tagdat <- read_excel('p:/obrien/biotelemetry/ocmd-bsb/data/taggingdata_ocbsb_16.xlsx')
 
 library(ggplot2)
-ggplot() + geom_histogram(data = tagdat, aes(x = `Length\r\n(TL, mm)`))
+# ggplot() + geom_histogram(data = tagdat, aes(x = `Length\r\n(TL, mm)`))
 
 
 source('p:/obrien/biotelemetry/ocmd-bsb/code/bsb_detection_import.R')
 
 library(dplyr)
-max.date <- filter(bsb.detects, grepl('Inn|Out|Mid', station)) %>%
+max.date <- filter(bsb.detects, grepl('Inn|Out|Mid', station),
+                   date.utc <= '2016-12-31') %>%
   group_by(transmitter) %>%
   summarize(max = max(date.utc)) %>%
-  mutate(array = ifelse(grepl('5[0-9]$|6[0-4]$', transmitter), 'Outer',
-                        ifelse(grepl('6[5-9]$|7[0-9]$', transmitter), 'Inner',
-                               'Middle')))
+  mutate(array = ifelse(grepl('5[0-9]$|6[0-4]$', transmitter), 'Southern',
+                        ifelse(grepl('6[5-9]$|7[0-9]$', transmitter), 'Northern',
+                               'Middle')),
+         array = ordered(array, levels = c('Northern', 'Middle', 'Southern')))
 
 library(lubridate)
 max.date$interval <- interval(ymd_hms('2016-06-09 00:00:00'), max.date$max)
 
-surv.all <- data.frame(date = seq.Date(ymd('2016-06-09'), ymd('2016-11-01'),
+surv.all <- data.frame(date = seq.Date(ymd('2016-06-09'), ymd('2016-12-31'),
                                    by = 'day'))
 surv.all$array <- 'All'
 surv.all$num <- sapply(surv.all$date,
@@ -33,26 +35,32 @@ surv.arr <- sapply(surv.arr, function(x) sapply(surv.all$date,
 surv.arr <- cbind(date = surv.all$date, as.data.frame(surv.arr))
 
 library(reshape2)
-surv.arr <- melt(surv.arr, id = 'date', measure = c('Inner', 'Middle', 'Outer'),
+surv.arr <- melt(surv.arr, id = 'date',
+                 measure = c('Northern', 'Middle', 'Southern'),
              variable.name = 'array', value.name = 'num')
 surv.arr$pct <- surv.arr$num/15
 
 surv.all <- rbind(surv.all, surv.arr)
+surv.all$array <- ordered(surv.all$array,
+                          levels = c('All', 'Northern', 'Middle', 'Southern'))
 
 ggplot() + geom_line(data = surv.all,
                      aes(x = date, y = num, col = array),
                      lwd = 2) +
   scale_color_manual(values = c('black', 'red', 'blue', 'purple')) +
-  labs(x = 'Date', y = 'Number remaining in array', color = 'Array') +
-  theme_bw() #+
+  labs(x = 'Date', y = 'Fish remaining in array', color = 'Array') +
+  scale_x_date(date_breaks = '1 month', date_labels = '%b', minor_breaks = NULL) +
+  theme_bw() +
+  theme(legend.position = c(0.9, 0.8),
+        legend.background = element_rect(color = 'black'))
 
 
 surv.all$doy <- yday(surv.all$date)
-lapply(unique(surv.all$array),
-       function(x) lm(num ~ doy,
+model_results <- lapply(unique(surv.all$array),
+       function(x) summary(lm(num ~ doy,
                       data = distinct(surv.all, array, num, .keep_all = T),
-                      subset = (array == x)))
+                      subset = (array == x))))
 
-summary(lm(num ~ doy * array,
+anova(lm(num ~ doy + array,
          data =  distinct(surv.all, array, num, .keep_all = T),
          subset = (array != 'All')))
