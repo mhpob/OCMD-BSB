d54dd8a66df82e58b568c00ad1c2184efbc05428
mhpob <mikeob9@gmail.com>
2017-08-02 16:12:14 -0400

    Adjust temp/noise/angle plot for manuscript.

--- Code/receiver_noise.R
+++ Code/receiver_noise.R
@@ -5,7 +5,7 @@ log <- read.csv('p:/obrien/biotelemetry/ocmd-bsb/vue_export.csv',
 log$Date.Time <- ymd_hms(log$Date.Time)
 log <- log %>%
   filter(grepl('Tilt|Average [depth|angle|temperature|noise]', Description),
-              Date.Time > '2016-06-12',
+              Date.Time >= '2016-06-13',
               Date.Time < '2016-12-31') %>%
   mutate(Date.Time = ceiling_date(Date.Time, unit = 'hour'),
          Data = as.numeric(Data))
@@ -24,45 +24,53 @@ label <- function(x){
   }
 
 log$site <- sapply(log$Receiver, label)
+log$array <- ifelse(grepl('Inner', log$site), 'Northern',
+                    ifelse(grepl('Outer', log$site), 'Southern', 'Middle'))
+log$array <- ordered(log$array, levels = c('Northern', 'Middle', 'Southern'))
 
+log <- log %>%
+  group_by(Date.Time, array, Description) %>%
+  summarize(value = mean(Data)) %>%
+  ungroup() %>%
+  mutate(array = ordered(array, levels = c('Northern', 'Middle', 'Southern')))
 
-library(TelemetryR)
-detects <- vemsort('p:/obrien/biotelemetry/ocmd-bsb/receiver logs')
-detects <- filter(detects,
-                  date.local > '2016-06-12',
-                  date.local < '2016-12-31',
-                  transmitter %in% paste0('A69-1601-', seq(44950, 44994, 1)))
+# library(TelemetryR)
+# detects <- vemsort('p:/obrien/biotelemetry/ocmd-bsb/receiver logs')
+# detects <- filter(detects,
+#                   date.local >= '2016-06-13',
+#                   date.local < '2016-12-31',
+#                   transmitter %in% paste0('A69-1601-', seq(44950, 44994, 1)))
 
-# ten_ceiling <- function(x){
-#   as.POSIXct(ceiling(as.numeric(x)/(10*60))*(10*60),
-#            origin='1970-01-01', tz = 'UTC')}
+## ten_ceiling <- function(x){
+##   as.POSIXct(ceiling(as.numeric(x)/(10*60))*(10*60),
+##            origin='1970-01-01', tz = 'UTC')}
 
-# detects$agg.date <- ten_ceiling(detects$date.utc)
-detects$agg.date <- ceiling_date(detects$date.local, unit = 'hour')
-names(detects)[2] <- 'Receiver'
+## detects$agg.date <- ten_ceiling(detects$date.utc)
+# detects$agg.date <- ceiling_date(detects$date.local, unit = 'hour')
+# names(detects)[2] <- 'Receiver'
 
-j <- distinct(detects, Receiver, transmitter, agg.date) %>%
-  group_by(Receiver,agg.date)
-k <- summarize(j, n=n())
+# j <- distinct(detects, Receiver, transmitter, agg.date) %>%
+#   group_by(Receiver,agg.date)
+# k <- summarize(j, n=n())
 
 library(ggplot2)
-temp.dat <- filter(log, grepl('SE', site),
+plot.dat <- filter(log,
                    Description == 'Average temperature' |
-                   Description == 'Maximum noise' & Data > 50 |
-                   Description == 'Tilt angle' & Data < 75)
-temp.dat$Description <- factor(temp.dat$Description,
-                               levels = c('Maximum noise', 'Tilt angle',
-                                          'Average temperature'))
+                   Description == 'Average noise' & value > 50 |
+                   Description == 'Tilt angle' & value < 75)
+plot.dat$Description <- ordered(plot.dat$Description,
+                               levels = c('Average temperature',
+                                          'Average noise', 'Tilt angle'))
 data_lab <- c(
   'Average temperature' = 'Temperature (°C)',
-  'Maximum noise' = 'Ambient Noise (mV)',
+  'Average noise' = 'Ambient Noise (mV)',
   'Tilt angle' = 'Tilt Angle (°)'
 )
 
 windows(11,7)
-ggplot() + geom_point(data = temp.dat,
-                       aes(x = Date.Time, y = Data)) +
-  facet_grid(Description ~ site, scales = 'free_y',
+ggplot() + geom_point(data = plot.dat,
+                       aes(x = Date.Time, y = value)) +
+  facet_grid(Description ~ array, scales = 'free_y',
              labeller = labeller(Description = data_lab)) +
   labs(x = 'Date', y = 'Value') +
   theme_bw()
