d76e3a69639b08588df305332539b8cc0b8552e0
mhpob <mikeob9@gmail.com>
2017-08-07 14:48:49 -0400

    Limit detection data to Dec 31, 2016.

--- Code/stayVgo.R
+++ Code/stayVgo.R
@@ -1,5 +1,5 @@
 library(readxl)
-tagdat <- read_excel('p:/obrien/biotelemetry/ocmd-bsb/data/taggingdata_ocbsb.xlsx')
+tagdat <- read_excel('p:/obrien/biotelemetry/ocmd-bsb/data/taggingdata_ocbsb_16.xlsx')
 
 library(ggplot2)
 ggplot() + geom_histogram(data = tagdat, aes(x = `Length\r\n(TL, mm)`)) +
@@ -10,7 +10,8 @@ ggplot() + geom_histogram(data = tagdat, aes(x = `Length\r\n(TL, mm)`)) +
 source('p:/obrien/biotelemetry/ocmd-bsb/code/bsb_detection_import.R')
 
 library(dplyr)
-max.date <- filter(bsb.detects, grepl('Inn|Out|Mid', station)) %>%
+max.date <- filter(bsb.detects, grepl('Inn|Out|Mid', station),
+                   date.local <= '2016-12-31') %>%
   group_by(transmitter) %>%
   summarize(max = max(date.utc))
 
@@ -21,6 +22,7 @@ tagdat$hermine <- ifelse(tagdat$max < '2016-09-03', 'Removed',
                          'Remained'))
 tagdat$hermine <- ordered(tagdat$hermine,
                           levels = c('Removed', 'Evacuated', 'Remained'))
+# group_by(tagdat, hermine) %>% summarize(n())
 
 ggplot() + geom_histogram(data = tagdat,
                           aes(`Length\r\n(TL, mm)`,
