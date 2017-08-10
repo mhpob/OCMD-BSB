cec13fdb134df48cbfee9cef10dde5d29f1c84a7
mhpob <mikeob9@gmail.com>
2017-08-10 15:31:05 -0400

    Adjust to import data from bsb_events.RDS

--- Code/receiver_summary.R
+++ Code/receiver_summary.R
@@ -1,42 +1,42 @@
 library(reshape2); library(lubridate); library(dplyr)
 
-log <- read.csv('p:/obrien/biotelemetry/ocmd-bsb/vue_export.csv',
-                stringsAsFactors = F)
+log <- readRDS('data/bsb_events.rds')
 
 slim.log <- filter(log, grepl('Tilt|Average [depth|temperature|noise]',
                               Description)) %>%
-  mutate(Date.Time = ymd_hms(Date.Time),
-         Data = as.numeric(Data),
+  mutate(Data = as.numeric(Data),
          Date.Time = ceiling_date(Date.Time, unit = 'day')) %>%
-  filter(Date.Time > ymd_hms('2016-06-12 23:59:59'))
+  filter(Date.Time >= '2016-06-13',
+         Date.Time <= '2016-12-31')
 
-slim.log <- dcast(slim.log, Date.Time + Receiver ~ Description,
+slim.log <- dcast(slim.log, Date.Time + Receiver + Array ~ Description,
            fun.aggregate = mean, value.var = 'Data')
 
 slim.log <- filter(slim.log, `Average seawater depth` > 15)
-slim.log <- melt(slim.log, id = c('Date.Time', 'Receiver'),
+pairs(slim.log[, c(1, 4:7)])
+
+slim.log <- melt(slim.log, id = c('Date.Time', 'Array', 'Receiver'),
                  variable.name = 'Description',
                  value.name = 'Data')
-slim.log <- mutate(slim.log,
-                   array = ifelse(grepl('[1|8|9]$', Receiver), 'Inner',
-                                  ifelse(grepl('[2|5|6]$', Receiver), 'Outer',
-                                         'Middle')))
 
-library(reshape2)
-avg.log <- group_by(slim.log, Date.Time, Receiver, Description) %>%
-  summarize(avg.data = mean(Data))
+slim.log$hermine <- ifelse(slim.log$Date.Time < '2016-09-03', 'pre', 'post')
 
-
-avg.log.wide <- dcast(avg.log, Date.Time + Receiver ~ Description,
-                      fun.aggregate = mean, value.var = 'avg.data')
-pairs(avg.log.wide[, c(1, 3:5)])
-
-
-summ.log <- group_by(slim.log, Receiver, array, Description) %>%
+summ.log <- group_by(slim.log, Array, Description, hermine) %>%
   summarize(min = min(Data),
             avg = mean(Data),
             std = sd(Data),
             max = max(Data))
 
+aov.log <- group_by(slim.log, Date.Time, Array, Description, hermine) %>%
+  summarize(mean = mean(Data)) %>%
+  mutate(doy = yday(Date.Time))
 
+wt.aov <- lm(sqrt(mean) ~ doy + Array, subset = (hermine == 'pre' &
+                                       Description == 'Average temperature'),
+                data = aov.log)
 
+summary(wt.aov)
+summary(aov(wt.aov))
+j <- TukeyHSD(aov(wt.aov), 'Array')
+plot(j)
+j
