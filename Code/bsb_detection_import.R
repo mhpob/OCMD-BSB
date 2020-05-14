c709dd9e4743489ce7c371eca40cc0d68d6eb76c
Mike O'Brien <mikeob9@gmail.com>
2020-05-14 19:00:45 -0400

    Add new years of transmitters.

--- Code/bsb_detection_import.R
+++ Code/bsb_detection_import.R
@@ -4,9 +4,15 @@ recieved.detects <- TelemetryR::vemsort('p:/obrien/biotelemetry/detections/recei
 
 bsb.detects <- rbind(offshore.detects, recieved.detects)
 bsb.detects <- bsb.detects[bsb.detects$transmitter %in%
-                             paste0('A69-1601-', seq(44950, 44994, 1)) |
-                             bsb.detects$transmitter %in%
-                             paste0('A69-1602-', seq(1411, 1448, 1)),]
+                             c(paste0('A69-1601-', seq(44950, 44994, 1)),
+                               paste0('A69-1602-', seq(1411, 1448, 1)),
+                               paste0('A69-1602-', seq(1449, 1455, 1)),
+                               paste0('A69-1602-', seq(9540, 9584, 1)),
+                               paste0('A69-1602-', seq(17709, 17724, 1)),
+                               paste0('A69-9006-', seq(7594, 7607, 1))),]
 rm(offshore.detects, recieved.detects)
 
-saveRDS(bsb.detects, file = 'data/bsb_detections.rds')
+
+
+
+saveRDS(bsb.detects, file = 'bsb_detections.rds')
